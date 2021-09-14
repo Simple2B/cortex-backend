19c303d64ee315bee9fcfa959cfe0891d8e2e1bd
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-14 11:49:52 +0000

    fix serveses

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from typing import List
 
-from app.services import AuthService, get_current_user
+from app.services import AuthService, get_current_doctor
 from app.schemas import DoctorCreate, Doctor, Token
 
 # from app import models, schemas
@@ -25,21 +25,21 @@ router = APIRouter(prefix="/auth")
 
 
 @router.post("/sign_up", response_model=Doctor, tags=["Auth"])
-async def sign_up(user_data: DoctorCreate):
+async def sign_up(doctor_data: DoctorCreate):
     """Register new user"""
     # TODO only admin could create user
     service = AuthService()
-    return await service.register_new_user(user_data)
+    return await service.register_new_user(doctor_data)
 
 
 @router.post("/sign_in", response_model=Token, tags=["Auth"])
 async def sign_in(form_data: DoctorCreate):
     """Login"""
     service = AuthService()
-    return await service.authenticate_user(form_data.username, form_data.password)
+    return await service.authenticate_doctor(form_data.email, form_data.password)
 
 
 @router.get("/doctor", response_model=Doctor, tags=["Auth"])
-def get_user(doctor: Doctor = Depends(get_current_user)):
+def get_user(doctor: Doctor = Depends(get_current_doctor)):
     """Show current authenticated doctor"""
     return doctor
