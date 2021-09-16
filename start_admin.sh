b85ffa0f2672438257b32fb09e1982cb71b54e71
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-16 09:35:35 +0000

    start sh files

---
+++ start_admin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /app
+export FLASK_APP=admin.run:app
+export FLASK_ENV=production
+export FLASK_DEBUG=0
+echo poetry run flask run -h 0.0.0.0
+poetry run flask db upgrade
+poetry run flask run -h 0.0.0.0 -p 5000
