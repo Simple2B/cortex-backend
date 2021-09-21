3806cabaa6d6fc4fd4e4b5e35768c76af1b26a02
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-21 06:42:59 +0000

    db session

--- app/database.py
+++ app/database.py
@@ -9,17 +9,19 @@ from app.logger import log
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URL, convert_unicode=True)
 
-db_session = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine)
-)
+
 Base = None
 if os.environ.get("FLASK_APP", None):
     log(log.INFO, "Use Flask")
     from admin import db
 
     Base = db.Model
+    db_session = db.session
 else:
     log(log.INFO, "Use FastAPI")
+    db_session = scoped_session(
+        sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    )
     Base = declarative_base()
     Base.query = db_session.query_property()
 
