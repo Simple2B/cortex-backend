44a2d2f3532f5e78c029e7e556f9a1fa3875c58e
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-21 12:22:57 +0000

    delete comment

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,28 +1,11 @@
 from fastapi import APIRouter, Depends
 
-# from typing import List
 
 from app.services import AuthService, get_current_doctor
 from app.schemas import DoctorCreate, Doctor, Token
 
-# from app import models, schemas
-
-# from sqlalchemy.orm import Session
-# from starlette.responses import RedirectResponse
-# from app.database import get_db
 
 router = APIRouter(prefix="/auth")
-
-
-# @router.get("/")
-# def main():
-#     return RedirectResponse(url="/docs/")
-
-
-# @router.get("/doctors/", response_model=List[schemas.Doctor])
-# def show_doctors(db: Session = Depends(get_db)):
-#     doctors = db.query(models.Doctor).all()
-#     return doctors
 
 
 @router.post("/sign_up", response_model=Doctor, tags=["Auth"])
