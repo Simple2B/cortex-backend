8ae5ef87825c26558ca08cdbe7e44eda35b25f11
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-21 12:23:32 +0000

    faild test fast api

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,21 +1,26 @@
+import os
 import pytest
 import asyncio
 from typing import Generator
 from fastapi.testclient import TestClient
 
-# from tortoise.contrib.test import finalizer, initializer
+# flake8: noqa E402
+os.environ["FLASK_APP"] = "testing"
+
+from app.database import engine, Base
 
 from app.setup import create_app
-from app.models import User
+from app.models import Doctor
 
 
 @pytest.fixture()
 def client() -> Generator:
     app = create_app()
-    # initializer(["app.models"], "sqlite://:memory:")
+    Base.metadata.create_all(bind=engine)
     with TestClient(app) as c:
         yield c
-    # finalizer()
+    Base.metadata.drop_all(bind=engine)
+    engine.dispose()
 
 
 @pytest.fixture()
@@ -24,11 +29,11 @@ def event_loop(client: TestClient) -> Generator:
 
 
 def test_auth(client: TestClient, event_loop: asyncio.AbstractEventLoop):
-    data = {"username": "michaelC", "password": "secret"}
+    data = {"email": "test@doctor.com", "password": "secret"}
     response = client.post("/auth/sign_up", json=data)
     assert response
     assert response.ok
-    user = event_loop.run_until_complete(User.filter(username=data["username"]).first())
+    user = event_loop.run_until_complete(Doctor.filter(email=data["email"]).first())
     assert user
     assert user.username == data["username"]
 
