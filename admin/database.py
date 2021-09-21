af4761d66ed8c9cddfb5514267847a429b79d453
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-21 07:00:23 +0000

    passwd admin

--- admin/database.py
+++ admin/database.py
@@ -9,7 +9,7 @@ def add_doctor_to_db(
     last_name=conf.ADMIN_LAST_NAME,
     email=conf.ADMIN_EMAIL,
     role=Doctor.DoctorRole.ADMIN,
-    passwd="123",
+    passwd="admin123",
 ):
     doctor = Doctor(
         first_name=first_name,
