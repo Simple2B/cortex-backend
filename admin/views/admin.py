5fc3114ae48520a5a237bce3b39d9204c099f109
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-21 08:24:25 +0000

    DoctorAdminModelView

--- admin/views/admin.py
+++ admin/views/admin.py
@@ -20,18 +20,15 @@ class PanelView(ModelView):
         doctor: Doctor = current_user
         return doctor.is_authenticated and doctor.role == Doctor.DoctorRole.ADMIN
 
-    # def inaccessible_callback(self):
-    #     # redirect to login page if user doesn't have access
-    #     return redirect(url_for("run.login", next=request.url))
-
 
 class DoctorAdminModelView(PanelView):
-    column_exclude_list = ["doctors", "clients"]
-    form_excluded_columns = ["doctors", "clients"]
+    column_exclude_list = ["doctors"]
+    form_excluded_columns = ["email_approved", "hash_password", "api_key"]
+    column_exclude_list = ["email_approved", "hash_password", "api_key"]
+
     column_searchable_list = ["first_name", "last_name", "email"]
     column_editable_list = ["role"]
     column_filters = ["email"]
     column_exclude_list = ["email"]
-    # can_create = False
-    # can_edit = False
+
     page_size = 15
