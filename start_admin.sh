98a3bcd7d84ff76c8fc67608c5539a4a9e421f57
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-23 07:38:02 +0000

    add environment to docker admin

--- start_admin.sh
+++ start_admin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export FLASK_APP=admin.run:app
-export FLASK_ENV=production
-export FLASK_DEBUG=0
+# export FLASK_APP=admin.run:app
+# export FLASK_ENV=production
+# export FLASK_DEBUG=0
 echo poetry run flask run -h 0.0.0.0
 poetry run flask db upgrade
 poetry run flask run -h 0.0.0.0 -p 5000
