e45259a4b970671799852b3e829eb731f1cb4d82
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-09-23 07:51:26 +0000

    add log

--- admin/views/mail_message.py
+++ admin/views/mail_message.py
@@ -21,31 +21,6 @@ def send_email(subject, sender, recipients: list[str], text_body, html_body):
     mail.send(msg)
 
 
-# def send_mail_to_user_without_passwd():
-#     doctors = Doctor.query.all()
-#     for doctor in doctors:
-#         if doctor.hash_password == None:
-#             email = doctor.email
-#             text_body = "Please confirm your mail!"
-#             html_body = ""
-#             send_email(
-#                 subject="hi",
-#                 sender=conf.MAIL_DEFAULT_SENDER,
-#                 recipients=[email],
-#                 text_body=text_body,
-#                 html_body=html_body,
-#             )
-#             log(log.INFO, "MAIL_MESSAGE: mail send successfully: [%s]", send_email)
-#             return redirect(url_for("doctor.index_view"))
-#         log(log.ERROR, "MAIL_MESSAGE: mail didn't send: [%s]", send_email)
-#         return redirect(url_for("doctor.index_view"))
-
-
-# @message_blueprint.route("/doctor/")
-# def index():
-#     send_mail_to_user_without_passwd()
-
-
 @message_blueprint.route("/doctor/new/", methods=["POST"])
 def new_doctor():
     form = NewDoctorForm()
@@ -75,7 +50,8 @@ def new_doctor():
             text_body=text_body,
             html_body=html_body,
         )
-        log(log.INFO, "MAIL_MESSAGE: mail send successfully: [%s]", send_email)
+        log(log.INFO, "NEW_DOCTOR: mail send successfully: [%s]", send_email)
         return redirect(url_for("doctor.index_view", next=request.url))
     else:
+        log(log.ERROR, "NEW_DOCTOR: can't validation form : [%s]", form.errors)
         return redirect(url_for("doctor.create_view", next=request.url))
