9d74ec75ddfd27759d5e98caaf21365d743ac70c
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-06 19:12:59 +0000

    test

--- tests/test_database.py
+++ tests/test_database.py
@@ -31,56 +31,6 @@ def client() -> Generator:
     engine.dispose()
 
 
-def test_client(client: TestClient):
-
-    TEST_CONDITIONS = ["Dizziness", "Asthma", "Obesity"]
-    TEST_DISEASES = ["Concussion", "Diabetes"]
-
-    data = {
-        "firstName": "Alex",
-        "lastName": "Brown",
-        "birthday": "2000-09-16",
-        "address": "Street",
-        "city": "New York",
-        "state": "US",
-        "zip": "02232",
-        "phone": "19077653340",
-        "email": "client@gmail.com",
-        "referring": "referring",
-        "conditions": TEST_CONDITIONS,
-        "otherCondition": "hvoroba",
-        "diseases": TEST_DISEASES,
-        "medications": "aspirin",
-        "covidTestedPositive": True,
-        "covidVaccine": False,
-        "stressfulLevel": 5,
-        "consentMinorChild": False,
-        "relationshipChild": "",
-    }
-
-    # 1. add Client into DB (client_data)
-    response = client.post("/api/client/registration", json=data)
-    assert response
-    assert response.ok
-
-    client = Client.query.first()
-    assert client
-    condition_names_in_db = [c.name for c in Condition.query.all()]
-    for condition_name in TEST_CONDITIONS:
-        assert condition_name in condition_names_in_db
-
-    disease_names_in_db = [d.name for d in Disease.query.all()]
-    for disease_name in TEST_DISEASES:
-        assert disease_name in disease_names_in_db
-
-    conditions = ClientCondition.query.filter(
-        ClientCondition.client_id == client.id
-    ).all()
-    assert len(conditions) == 4
-    desesses = ClientDisease.query.filter(ClientDisease.client_id == client.id).all()
-    assert len(desesses) == 2
-
-
 def test_generate_data(client: TestClient):
     generate_test_data()
 
