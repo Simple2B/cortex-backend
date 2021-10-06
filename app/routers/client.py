bf9e51f79d5b07a722a9e9d397ada63eea8eed51
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-06 19:09:15 +0000

    routes  get clients for queue, add client to queue, show clients for queue

--- app/routers/client.py
+++ app/routers/client.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from typing import List
+from fastapi.security import OAuth2PasswordBearer
 
-from app.services import ClientService
-from app.schemas import ClientInfo, Client
+from app.services import ClientService, QueueService
+from app.schemas import ClientInfo, Client, QueueMember, Queue
+from app.models import Client as ClientDB, QueueMember as QueueMemberDB
 
 
 router_client = APIRouter(prefix="/client")
@@ -15,3 +18,25 @@ async def registrations(client_data: ClientInfo):
     if not client:
         raise HTTPException(status_code=404, detail="Client didn't registration")
     return client
+
+
+@router_client.get("/clients", response_model=List[Client], tags=["Client"])
+def get_clients():
+    """Show clients"""
+    return ClientDB.query.all()
+
+
+@router_client.post("/add_clients_queue", response_model=str, tags=["Client"])
+async def add_client_to_queue(client_data: ClientInfo):
+    """Put clients to queue"""
+    service = QueueService()
+    service.add_client_to_queue(client_data)
+    # if not client:
+    #     raise HTTPException(status_code=404, detail="Client didn't add to queue")
+    return "ok"
+
+
+@router_client.get("/queue", response_model=List[Queue], tags=["Client"])
+def get_queue():
+    """Show queue"""
+    return QueueMemberDB.query.all()
