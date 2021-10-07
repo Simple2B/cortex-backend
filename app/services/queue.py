72b890bcdb1b5e2c6077c50f4b2e822ded5090e2
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-07 09:12:25 +0000

    delete space

--- app/services/queue.py
+++ app/services/queue.py
@@ -14,21 +14,14 @@ from app.logger import log
 
 class QueueService:
     def add_client_to_queue(self, client_data: Client, doctor: Doctor) -> Client:
-
         doctor = DoctorDB.query.filter(DoctorDB.email == doctor.email).first()
-
         client = ClientDB.query.filter(ClientDB.phone == client_data.phone).first()
-
         if not client:
             return
-
         reception = Reception.query.filter(Reception.doctor_id == doctor.id).first()
         reception.date = datetime.datetime.now()
-
         reception.save()
-
         log(log.INFO, "Reception created [%d]", reception.id)
-
         visit = Visit.query.filter(Visit.client_id == client.id).first()
         if not visit:
             visit = Visit(
@@ -39,16 +32,13 @@ class QueueService:
                 doctor_id=doctor.id,
             )
             visit.save()
-
             log(log.INFO, "Visit created [%d]", visit.id)
         else:
             # ? duration => may be end of visit
             # visit.rougue_mode=,
             visit.data_time = datetime.datetime.now()
             visit.doctor_id = doctor.id
-
             visit.save()
-
         queue_member = QueueMember(
             # place_in_queue=,
             # canceled=,
@@ -56,7 +46,5 @@ class QueueService:
             visit_id=visit.id,
             reception_id=reception.id,
         )
-
         queue_member.save()
-
         log(log.INFO, "QueueMember created [%d]", queue_member.id)
