a4c96e852a3e5447bfd985ce0750db5524103e09
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-07 09:04:57 +0000

    test login

--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,15 @@
-def login(client, email, password="123"):
-    return client.post(
-        "/admin/login", data=dict(email=email, password=password), follow_redirects=True
-    )
+from admin.config import BaseConfig as conf
+
+
+def login(client, email=conf.ADMIN_EMAIL, password=conf.ADMIN_PASSWORD) -> str:
+    """get token"""
+    data = dict(email=email, password=password)
+    response = client.post("/api/auth/sign_in", json=data)
+    assert response
+    assert response.ok
+    assert b'"access_token"' in response.content
+    return response.json()["access_token"]
 
 
 def logout(client):
-    return client.get("/admin/logout", follow_redirects=True)
+    raise NotImplementedError()
