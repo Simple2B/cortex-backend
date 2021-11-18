132b1d93c2b3ff73a69ec55505e7a2211f9cf365
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-18 14:17:11 +0000

    add logs

--- app/services/visit.py
+++ app/services/visit.py
@@ -1,7 +1,5 @@
-import datetime
 from typing import List
 from fastapi import HTTPException, status
-from sqlalchemy.sql.elements import and_
 from app.schemas import Doctor, VisitHistory
 from app.models import Client as ClientDB
 from app.logger import log
@@ -10,5 +8,14 @@ from app.logger import log
 class VisitService:
     def get_history_visit(self, api_key: str, doctor: Doctor) -> List[VisitHistory]:
         client: ClientDB = ClientDB.query.filter(ClientDB.api_key == api_key).first()
+        if not client:
+            log(log.ERROR, "get_history_visit: Client [%s] not found", client)
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Client not found"
+            )
+
+        log(log.INFO, "get_history_visit: Client [%s]", client)
         visits = client.client_info["visits"]
+
+        log(log.INFO, "get_history_visit: Count [%d] of visits ", len(visits))
         return visits
