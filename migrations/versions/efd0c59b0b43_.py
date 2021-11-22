08d2e3f6ed9bde1524f72683fd1ee67d2ec2fc3c
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-22 15:31:04 +0000

    flake8

--- migrations/versions/efd0c59b0b43_.py
+++ migrations/versions/efd0c59b0b43_.py
@@ -1,16 +1,16 @@
 """empty message
 
 Revision ID: efd0c59b0b43
-Revises: 
+Revises:
 Create Date: 2021-11-22 14:27:22.145601
 
 """
-from alembic import op
-import sqlalchemy as sa
+from alembic import op  # noqa F401
+import sqlalchemy as sa  # noqa F401
 
 
 # revision identifiers, used by Alembic.
-revision = 'efd0c59b0b43'
+revision = "efd0c59b0b43"
 down_revision = None
 branch_labels = None
 depends_on = None
