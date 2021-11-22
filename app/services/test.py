1e92321c001a01151e69be16320243c4f73f0ded
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-22 12:56:09 +0000

    services

---
+++ app/services/test.py
@@ -0,0 +1,43 @@
+import datetime
+from typing import List
+
+from fastapi import HTTPException, status
+from sqlalchemy.sql.elements import and_
+
+from app.schemas import Doctor, PostTest, CreateTest
+from app.models import Client as ClientDB, Test
+from app.logger import log
+
+
+class TestService:
+    def create_test(self, data: PostTest, doctor: Doctor) -> CreateTest:
+        client: ClientDB = ClientDB.query.filter(
+            ClientDB.api_key == data.api_key
+        ).first()
+
+        if not client:
+            log(log.ERROR, "Client not found")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Client not found"
+            )
+
+        log(log.INFO, "Client [%s] for test", client)
+
+        date = datetime.datetime.strptime(data.date, "%m/%d/%Y, %H:%M:%S")
+
+        if not date:
+            log(log.ERROR, "Date not found")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Front end Data don't send",
+            )
+
+        log(log.INFO, "Date [%s] for start test", date)
+
+        create_test = Test(
+            date=date,
+            client_id=client.id,
+            doctor_id=doctor.id,
+        ).save()
+
+        return create_test
