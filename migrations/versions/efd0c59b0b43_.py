107edc65f3171612b08c47040f8be2ed0c9ac714
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-22 14:27:44 +0000

    migration update

---
+++ migrations/versions/efd0c59b0b43_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: efd0c59b0b43
+Revises: 
+Create Date: 2021-11-22 14:27:22.145601
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'efd0c59b0b43'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
