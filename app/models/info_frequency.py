cc438f518abe0662d09ed8f5492132a4fcde075a
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-29 10:43:58 +0000

    add to info doctor_id

--- app/models/info_frequency.py
+++ app/models/info_frequency.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey, Column, Integer, String
 
 from app.database import Base
 from .utils import ModelMixin
+
+from sqlalchemy.orm import relationship
 
 
 class InfoFrequency(Base, ModelMixin):
@@ -9,6 +11,10 @@ class InfoFrequency(Base, ModelMixin):
 
     id = Column(Integer, primary_key=True, index=True)
     frequency = Column(String(128), nullable=True)
+
+    doctor_id = Column(Integer, ForeignKey("doctors.id"))
+
+    doctor = relationship("Doctor", viewonly=True)
 
     def __repr__(self):
         return f"<{self.id}: c:{self.frequency}>"
