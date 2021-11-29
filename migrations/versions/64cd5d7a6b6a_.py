e140b060a311eefe441eb1a72cea487429130d5b
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-29 07:36:00 +0000

    migration

--- migrations/versions/64cd5d7a6b6a_.py
+++ migrations/versions/64cd5d7a6b6a_.py
@@ -5,13 +5,13 @@ Revises: c3e0e24f42b7
 Create Date: 2021-11-27 10:59:15.620849
 
 """
-from alembic import op
-import sqlalchemy as sa
+from alembic import op  # noqa: F401
+import sqlalchemy as sa  # noqa: F401
 
 
 # revision identifiers, used by Alembic.
-revision = '64cd5d7a6b6a'
-down_revision = 'c3e0e24f42b7'
+revision = "64cd5d7a6b6a"
+down_revision = "c3e0e24f42b7"
 branch_labels = None
 depends_on = None
 
