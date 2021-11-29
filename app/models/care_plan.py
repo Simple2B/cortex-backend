754ee58cfe82aedc9dabe8fb28520cb8b0ff874a
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-29 15:39:54 +0000

    care plan model

---
+++ app/models/care_plan.py
@@ -0,0 +1,25 @@
+import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, DateTime, String
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+from .utils import ModelMixin
+
+
+class CarePlan(Base, ModelMixin):
+    __tablename__ = "care_plans"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DateTime, default=datetime.datetime.today)
+    care_plan = Column(String(128), nullable=True)
+    frequency = Column(String(128), nullable=True)
+
+    client_id = Column(Integer, ForeignKey("clients.id"))
+    doctor_id = Column(Integer, ForeignKey("doctors.id"))
+
+    client = relationship("Client", viewonly=True)
+    doctor = relationship("Doctor", viewonly=True)
+
+    def __repr__(self):
+        return f"<{self.id}: c:{self.client_id}-d:{self.doctor_id}>"
