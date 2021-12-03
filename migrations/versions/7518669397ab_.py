4d80abf050035c78a740af276ee77a7bc6df8fd6
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-12-03 15:17:16 +0000

    migration

---
+++ migrations/versions/7518669397ab_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7518669397ab
+Revises: f2e93fd1a1f6
+Create Date: 2021-12-03 15:16:26.839213
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7518669397ab'
+down_revision = 'f2e93fd1a1f6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
