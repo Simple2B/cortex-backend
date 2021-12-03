5d767f92db8a1578f1b50b1ba4f38f45018d8cd9
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-12-03 15:30:31 +0000

    flake

--- migrations/versions/206bd8a5995e_.py
+++ migrations/versions/206bd8a5995e_.py
@@ -1,16 +1,16 @@
 """empty message
 
 Revision ID: 206bd8a5995e
-Revises: 
+Revises:
 Create Date: 2021-12-03 12:07:39.614480
 
 """
-from alembic import op
-import sqlalchemy as sa
+from alembic import op  # noqa F401
+import sqlalchemy as sa  # noqa F401
 
 
 # revision identifiers, used by Alembic.
-revision = '206bd8a5995e'
+revision = "206bd8a5995e"
 down_revision = None
 branch_labels = None
 depends_on = None
