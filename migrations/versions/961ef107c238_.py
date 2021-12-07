89ff1f50e465672726c1c3d5f745c1f61015be1f
Aleksandr <amvorobei@gmail.com>
2021-12-07 17:58:29 +0200

    New migrations

---
+++ migrations/versions/961ef107c238_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 961ef107c238
+Revises: 188282308e9a
+Create Date: 2021-12-07 14:09:53.057518
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '961ef107c238'
+down_revision = '188282308e9a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
