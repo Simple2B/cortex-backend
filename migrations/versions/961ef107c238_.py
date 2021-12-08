b3c597030635bcebb4394c5101ed8d4eed62b4d1
Aleksandr <amvorobei@gmail.com>
2021-12-08 02:19:44 +0200

    Fix migration

--- migrations/versions/961ef107c238_.py
+++ migrations/versions/961ef107c238_.py
@@ -1,7 +1,7 @@
 """empty message
 
 Revision ID: 961ef107c238
-Revises: 188282308e9a
+Revises: 7518669397ab
 Create Date: 2021-12-07 14:09:53.057518
 
 """
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '961ef107c238'
-down_revision = '188282308e9a'
+revision = "961ef107c238"
+down_revision = "7518669397ab"
 branch_labels = None
 depends_on = None
 
