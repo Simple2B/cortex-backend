1af95702c8a6b6be764016994cb12b21dc48a868
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-01-17 10:35:31 +0000

    test

--- admin/__init__.py
+++ admin/__init__.py
@@ -17,21 +17,22 @@ mail = Mail()
 migrate = Migrate()
 admin = None
 
+# for deploy
 
-class PrefixMiddleware(object):
-    def __init__(self, app, prefix=""):
-        self.app = app
-        self.prefix = prefix
+# class PrefixMiddleware(object):
+#     def __init__(self, app, prefix=""):
+#         self.app = app
+#         self.prefix = prefix
 
-    def __call__(self, environ, start_response):
+#     def __call__(self, environ, start_response):
 
-        if environ["PATH_INFO"].startswith(self.prefix):
-            environ["PATH_INFO"] = environ["PATH_INFO"][len(self.prefix)]
-            environ["SCRIPT_NAME"] = self.prefix
-            return self.app(environ, start_response)
-        else:
-            start_response("404", [("Content-Type", "text/plain")])
-            return ["This url does not belong to the app.".encode()]
+#         if environ["PATH_INFO"].startswith(self.prefix):
+#             environ["PATH_INFO"] = environ["PATH_INFO"][len(self.prefix)]
+#             environ["SCRIPT_NAME"] = self.prefix
+#             return self.app(environ, start_response)
+#         else:
+#             start_response("404", [("Content-Type", "text/plain")])
+#             return ["This url does not belong to the app.".encode()]
 
 
 def create_app(environment="development"):
@@ -49,7 +50,10 @@ def create_app(environment="development"):
 
     # Instantiate app.
     app = Flask(__name__, static_url_path="/admin/flask/static")
-    app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix="/api")
+
+    # for deploy
+    # app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix="/api")
+
     app.config["APPLICATION_ROOT"] = "/api"
     app.config["SCRIPT_NAME"] = "/api"
     # app.config["SERVER_NAME"] = "/api"
