b4842a5161377b3aa5d9d900a5c3e4e48671f7d7
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-01-17 10:09:24 +0000

    for deploy

--- start_admin.sh
+++ start_admin.sh
@@ -5,3 +5,6 @@
 echo poetry run flask run -h 0.0.0.0
 poetry run flask db upgrade
 poetry run flask run -h 0.0.0.0 -p 5000
+
+#for deploy
+# poetry run gunicorn admin.run:app -b :5000 --log-level debug
