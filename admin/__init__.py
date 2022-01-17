cccfe5483238c44e436967db0afa53ee8a204dba
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-01-17 09:51:03 +0000

    PrefixMiddleware

--- admin/__init__.py
+++ admin/__init__.py
@@ -18,6 +18,22 @@ migrate = Migrate()
 admin = None
 
 
+class PrefixMiddleware(object):
+    def __init__(self, app, prefix=""):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ["PATH_INFO"].startswith(self.prefix):
+            environ["PATH_INFO"] = environ["PATH_INFO"][len(self.prefix) :]
+            environ["SCRIPT_NAME"] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response("404", [("Content-Type", "text/plain")])
+            return ["This url does not belong to the app.".encode()]
+
+
 def create_app(environment="development"):
     global admin
     from admin.config import config
@@ -32,7 +48,11 @@ def create_app(environment="development"):
     from admin.views import CortexAdminIndexView, DoctorAdminModelView
 
     # Instantiate app.
-    app = Flask(__name__, static_url_path="/flask/static")
+    app = Flask(__name__, static_url_path="/admin/flask/static")
+    app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix="/api")
+    app.config["APPLICATION_ROOT"] = "/api"
+    app.config["SCRIPT_NAME"] = "/api"
+    # app.config["SERVER_NAME"] = "/api"
 
     # Set app config.
     env = os.environ.get("FLASK_ENV", environment)
@@ -64,9 +84,11 @@ def create_app(environment="development"):
         index_view=CortexAdminIndexView(),
         name="Cortex",
         template_mode="bootstrap3",
+        # url="/api/admin",
     )
 
     admin.add_view(DoctorAdminModelView(Doctor, db.session))
     admin.add_view(ModelView(Client, db.session))
+    # print(app.url_map)
 
     return app
