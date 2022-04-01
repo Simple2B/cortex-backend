ebe747f5b2ef07a3e6c7fbc0628c85d4d58f73e5
AlexAzalor <yablunovsky.a@gmail.com>
2022-04-01 12:36:47 +0300

    flake8 style fix

--- migrations/versions/4ba8a0a05a99_.py
+++ migrations/versions/4ba8a0a05a99_.py
@@ -5,9 +5,6 @@ Revises: a5dc50d00b70
 Create Date: 2022-03-31 15:19:55.000161
 
 """
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision = '4ba8a0a05a99'
